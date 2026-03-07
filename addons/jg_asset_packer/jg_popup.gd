2e8488f6239e9ab0dca03ab9dcf4b7477738c62d
John Grey <johngrey4296@gmail.com>
2026-03-07 20:20:26 +0000

    [refactor]: popup to ConfirmationDialog

--- addons/jg_asset_packer/jg_popup.gd
+++ addons/jg_asset_packer/jg_popup.gd
@@ -1,20 +1,46 @@
 #  naming.gd -*- mode: gdscript -*-
 @tool
 class_name JGPopup
-extends Popup
+extends ConfirmationDialog
 """
 
 """
 
-signal addon_named(text:String)
+static var default_name		: String	= "MyCollectedAddon"
+static var default_debug	: int		= 0
+static var default_prefix	: String	= "res://addons/"
+
+signal addon_named(text:String, debug:int)
 
 
 func _init():
 	close_requested.connect(hide)
 
 func _ready() -> void:
-	find_child("LineEdit").text_submitted.connect(text_submit)
+	$control/name/value.placeholder_text	= JGPopup.default_name
+	$control/prefix/value.placeholder_text  = JGPopup.default_prefix
+	$control/debug/value.value				= JGPopup.default_debug
+	confirmed.connect(complete)
 
-func text_submit(text):
-	addon_named.emit(text)
+func complete():
+	JGPopup.default_debug	= $control/debug/value.value
+	match $control/prefix/value.text:
+		"":
+			pass
+		var prefix:
+			JGPopup.default_prefix  = $control/prefix/value.text
+
+	match $control/name/value.text:
+		"":
+			pass
+		var text:
+			JGPopup.default_name = text
+
+	print("Values: %s %s" % [JGPopup.default_prefix.path_join(JGPopup.default_name), JGPopup.default_debug])
+	addon_named.emit(
+		JGPopup.default_name,
+		JGPopup.default_prefix,
+		JGPopup.default_debug,
+		)
+
 	close_requested.emit()
