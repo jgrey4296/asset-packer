c059fcde53406048cb638afdaef0a3fd5bd8f6d4
John Grey <johngrey4296@gmail.com>
2026-03-07 20:19:05 +0000

    [remove]: example in addons

--- addons/jg_asset_packer/jgae_activator.gd
+++ addons/jg_asset_packer/jgae_activator.gd
@@ -11,9 +11,11 @@ var st_slot = EditorContextMenuPlugin.CONTEXT_SLOT_SCENE_TREE
 var scene_menu = preload("jg_scene_menu.gd").new()
 
 func _enter_tree():
+	print("Activating jg_asset_packer")
 	add_context_menu_plugin(fs_slot, fs_menu)
 	add_context_menu_plugin(st_slot, scene_menu)
 
 func _exit_tree():
+	print("DeActivating jg_asset_packer")
 	remove_context_menu_plugin(fs_menu)
 	remove_context_menu_plugin(scene_menu)
